613e069fa1442815264d56996602239d9aa70b58
Dane Lipscombe <danelipscombe@gmail.com>
2026-01-15 14:50:07 +1100

    mcu

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 npm exec ergogen -- .
-cp output/pcbs/tenshi.kicad_pcb kicad/
+cp output/pcbs/koumori.kicad_pcb kicad/
 # for i in output/cases/*.jscad; do npm exec openjscad -- "$i" -of stla; done
 # npm exec openjscad -- output/cases/caseLeft.jscad -of stla -o case/caseLeft.stl
 # npm exec openjscad -- output/cases/caseRight.jscad -of stla -o case/caseRight.stl
